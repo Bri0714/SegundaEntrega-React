b3647def8171968f7a4ec350213263b630e3576e
Brian Alexander Amezquita Parada <bap00258@hotmail.com>
2023-03-09 16:43:22 -0500

    agregando ultimos detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import axios from 'axios'
 import Home from './components/Home/Home'
 import './App.css'
@@ -26,9 +26,11 @@ function App() {
       <div>
         <h1 className='text-center title'> FABY SPORTS </h1>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Navigate to= "Home" />}/>
+          <Route path="/Home" element={<Home/>}/>
           <Route path="/productos" element={<ProductList productos={productos}/>}/>
           <Route path="/productos/:id" element={<ProductItem productos={productos}/>}/>
+          <Route path="*" element={<h1>404 Not Found</h1>}/>
         </Routes>
       </div>
     </body>
